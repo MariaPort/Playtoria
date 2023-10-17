32c01a7b810c1c35ae92171687c972c7168a0f97
Mariia Portnova <portnovamariia@gmail.com>
2023-10-17 15:47:25 +0400

    fixes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://playtoria.com',
+    siteUrl: process.env.SITE_URL || 'https://www.playtoria.com',
     generateRobotsTxt: true,
     sitemapSize: 5000,
     generateIndexSitemap: false,
