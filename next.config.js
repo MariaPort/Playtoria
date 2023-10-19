c3c23612465ebf305ccfc123dd416c8481fe0a7f
Mariia Portnova <portnovamariia@gmail.com>
2023-10-19 19:53:32 +0400

    Small fixes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    assetPrefix: ".",
+    assetPrefix: "/",
     modularizeImports: {
       '@mui/icons-material': {
         transform: '@mui/icons-material/{{member}}',
